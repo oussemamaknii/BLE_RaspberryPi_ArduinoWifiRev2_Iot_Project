b0476380691efec4e8275b7dae401f3b49b581d3
oussema makni <58178524+oussemamaknii@users.noreply.github.com>
2022-05-25 10:10:14 +0100

    updqte chenges

--- sketch.ino
+++ sketch.ino
@@ -6,23 +6,12 @@
 #include <TinyGPS.h>
 #include <Arduino_LSM6DS3.h>
 
+#define NUMBER_OF_SENSORS 4
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 9, d7 = 8;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(12, 11, 5, 4, 9, 8);
 float latitude, longitude;
 SoftwareSerial gpSerial(2, 3);
 TinyGPS gps;
-
-//----------------------------------------------------------------------------------------------------------------------
-// BLE UUIDs
-//----------------------------------------------------------------------------------------------------------------------
-
-#define BLE_UUID_SENSOR_DATA_SERVICE "8158b2fd-94e4-4ff5-a99d-9a7980e998d7"
-#define BLE_UUID_MULTI_SENSOR_DATA "d508bc0a-ecdd-5ba5-9d33-13d5887a7718"
-#define BLE_UUID_PASSWORD "7dfe60ef-175f-5a61-ac33-8a658b07cdb7"
-
-#define NUMBER_OF_SENSORS 3
-
 uint32_t step = 0;
 
 union multi_sensor_data {
@@ -42,13 +31,13 @@ union Response {
 union multi_sensor_data multiSensorData;
 uint8_t check[sizeof("password22")];
 uint8_t value[sizeof("password22")];
-bool once = true;
 union Response response;
+boolean fall = false;
 
 
-BLEService sensorDataService(BLE_UUID_SENSOR_DATA_SERVICE);
-BLECharacteristic multiSensorDataCharacteristic(BLE_UUID_MULTI_SENSOR_DATA, BLERead | BLENotify, sizeof multiSensorData.bytes);
-BLECharacteristic passwordCharacteristic(BLE_UUID_PASSWORD, BLERead | BLEWrite | BLENotify, sizeof check);
+BLEService sensorDataService("8158b2fd-94e4-4ff5-a99d-9a7980e998d7");
+BLECharacteristic multiSensorDataCharacteristic("d508bc0a-ecdd-5ba5-9d33-13d5887a7718", BLERead | BLENotify, sizeof multiSensorData.bytes);
+BLECharacteristic passwordCharacteristic("7dfe60ef-175f-5a61-ac33-8a658b07cdb7", BLERead | BLEWrite | BLENotify, sizeof check);
 
 void setupSensor() {
   bma456.initialize(RANGE_4G, ODR_1600_HZ, NORMAL_AVG4, CONTINUOUS);
@@ -58,9 +47,8 @@ void setupSensor() {
 
   gpSerial.begin(9600);
 
-  // if (IMU.begin()) {
-  //   Serial.println("completed successfully.");
-  // }
+  if (IMU.begin()) {
+  }
 }
 
 bool setupBleMode() {
@@ -86,79 +74,62 @@ void setup() {
 
   setupSensor();
 
-  if (setupBleMode()) {
-    Serial.println(BLE.address());
-    // multiSensorData.values[0] = 15.26;
-  }
+  setupBleMode();
 }
 
 void loop_sensors() {
   step = bma456.getStepCounterOutput();
-  // Serial.print("step : ");
-  // Serial.println(step);
 
-  lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("STEP : ");
-  lcd.setCursor(0, 6);
   lcd.print(step);
+  lcd.setCursor(0, 6);
+  lcd.print("Fall : False");
 
   if (gps.encode(gpSerial.read())) {
     gps.f_get_position(&latitude, &longitude);
     Serial.print(latitude);
     Serial.println(longitude);
-    multiSensorData.values[1] = longitude;
-    multiSensorData.values[2] = latitude;
+    if (longitude == 0 & latitude == 0) {
+      multiSensorData.values[1] = 48.8246997;
+      multiSensorData.values[2] = 2.2802696;
+    } else {
+      multiSensorData.values[1] = longitude;
+      multiSensorData.values[2] = latitude;
+    }
   }
-
-  //  char buffer[8];    // string buffer for use with dtostrf() function
-  //  float ax, ay, az;  // accelerometer values
-  //  float gx, gy, gz;  // gyroscope values
-
-  //  // Retrieve and print IMU values
-  //  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()
-  //     && IMU.readAcceleration(ax, ay, az) && IMU.readGyroscope(gx, gy, gz)) {
-  //       multiSensorData.values[3] = dtostrf(ax, 4, 1, buffer);
-  //       multiSensorData.values[4] = dtostrf(ay, 4, 1, buffer);
-  //       multiSensorData.values[5] = dtostrf(az, 4, 1, buffer);
-  //       multiSensorData.values[6] = dtostrf(gx, 4, 1, buffer);
-  //       multiSensorData.values[7] = dtostrf(gy, 4, 1, buffer);
-  //       multiSensorData.values[8] = dtostrf(gz, 4, 1, buffer);
-  //  }
+  imu_loop();
 }
 
+void imu_loop() {
+  
+}
 
 void loop() {
-  static long previousMillis = 0;
 
   loop_sensors();
 
   BLEDevice central = BLE.central();
 
   if (central) {
-
-    Serial.print("Incoming connection from: ");
-    Serial.println(central.address());
-
     while (central.connected()) {
       long interval = 2000;
       unsigned long currentMillis = millis();
+      static long previousMillis = 0;
 
       if (currentMillis - previousMillis > interval) {
         if (passwordCharacteristic.written() == 1) {
           passwordCharacteristic.readValue(value, 10);
           if (memcmp(check, value, sizeof(check)) == 0) {
             response.value = 1;
-            Serial.println("yo");
             passwordCharacteristic.writeValue(response.bytes, sizeof response.bytes);
           } else {
             response.value = 0;
-            Serial.println("yoo");
             passwordCharacteristic.writeValue(response.bytes, sizeof response.bytes);
           }
         }
         if (memcmp(check, value, sizeof(check)) == 0) {
           multiSensorData.values[0] = step;
-          Serial.println("Sending");
           multiSensorDataCharacteristic.writeValue(multiSensorData.bytes, sizeof multiSensorData.bytes);
         } else {
           response.value = 0;
